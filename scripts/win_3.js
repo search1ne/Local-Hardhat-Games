67af019dcc3cc10279a5caa2f0b306333a6115ef
searchone <plo.cryptocartel@protonmail.com>
2023-01-25 11:27:45 -0800

    solutions

--- scripts/win_3.js
+++ scripts/win_3.js
@@ -1,13 +1,13 @@
 // add the game address here and update the contract name if necessary
-const gameAddr = "";
-const contractName = "Game1";
+// const gameAddr = "0x0165878A594ca255338adfa4d48449f69242Eb8F";
+const contractName = "Game3";
 
 async function main() {
     // attach to the game
     const game = await hre.ethers.getContractAt(contractName, gameAddr);
 
     // do whatever you need to do to win the game here:
-    const tx = await game.win();
+    const tx = await game.win(45);
 
     // did you win? Check the transaction receipt!
     // if you did, it will be in both the logs and events array
